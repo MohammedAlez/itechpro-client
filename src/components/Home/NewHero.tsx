c267145f92df2917c8508f3aa0451b4b6048db25
Bouazza Mohammed alez <mohammed.alez@yahoo.com>
2025-05-25 09:22:08 +0100

    fix: ui 2

--- src/components/Home/NewHero.tsx
+++ src/components/Home/NewHero.tsx
@@ -14,9 +14,10 @@ import { SERVER_URL } from "@/utils";
 
 
 const banners = [
+    '/images/banners/baner-3.jpg',
     '/images/banners/baner-2.jpg',
     '/images/banners/baner-1.jpg',
-    '/images/banners/baner-3.jpg',
+    '/images/banners/baner-4.jpg',
     '/images/banners/baner-5.jpg',
     
 ]
